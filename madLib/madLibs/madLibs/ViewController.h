10d70f18bccfc2f3f157e7daf987f1810ee342d7
Daniel Distant <daniel.distant1@gmail.com>
2015-06-07 14:32:51 -0400

    Initial Commit

---
+++ madLib/madLibs/madLibs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  madLibs
+//
+//  Created by Daniel Distant on 6/7/15.
+//  Copyright (c) 2015 ddistant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
